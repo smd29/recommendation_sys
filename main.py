0fe8648bfb6655e7eafb18821e94c5ffc5c8d1aa
smd29 <shreyasdas29@yahoo.com>
2019-04-06 12:17:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,53 +1,45 @@
-import eucledian_distance
+# import eucledian_distance
 import users_rating
-#from math import sqrt
+# # from math import sqrt
 import random
-#import matching_similarity
-import user_preference_data
-#import word_similarity
-#import itertools
+# # import matching_similarity
+# import user_preference_data
+# import word_similarity
+# import itertools
 import csv
 
-
-
-#consider location is the 1st attribute, flat_type is 2nd and flat_furnishing is 3rd
+# consider location is the 1st attribute, flat_type is 2nd and flat_furnishing is 3rd
 if __name__ == '__main__':
-    Assigned_house=open('Assigned_houses.csv')
-    csv_f=csv.reader(Assigned_house)
-    user_id=[]    
-    price=[]
-    location=[]
-    size=[]
-    flat_type=[]
-    flat_id=[]
+    Assigned_house = open(r"C:\Users\nEW u\Desktop\Recommendation system\Implementation\DataSet\Assigned_houses.csv")
+    csv_f = csv.reader(Assigned_house)
+    user_id = []
+    price = []
+    location = []
+    size = []
+    flat_type = []
+    flat_id = []
+    # users = sum(1 for row in Assigned_house)
+    # print(users)
     for column in csv_f:
-         user_id.append(column[1])
-         location.append(column[2])
-         price.append(column[3])
-         size.append(column[4])
-         flat_type.append(column[5])
-         flat_id.append(column[6]   
-  
-  
+        user_id.append(column[0])
+        location.append(column[1])
+        price.append(column[2])
+        size.append(column[3])
+        flat_type.append(column[4])
+        flat_id.append(column[5])
+    # print(size)
     price.remove('Price')
-    location.remove('Location')
-    size.remove('size')
+    location.remove('Location ')
+    size.remove('Size')
     flat_type.remove('type')
     flat_id.remove('Flat id')
-    user_id.remove('User')                    
+    user_id.remove('User')
+    users = len(user_id)
+    attributes = 4
+    print(users)
 
-        # existing_users_rating = [ [] for i in range(users)]
-    # for i in existing_users_rating:
-    #     for x in range(attributes):
-    #         i.append(random.randint(0, 5))
-    # print("The preference factors for the existing users are: " + str(existing_users_rating))  ##this user_list is the list of list
 
-    # new_user_rating = []
-    # for x in range(attributes):
-    #     new_user_rating.append(random.randint(0,5))
-    # print("The preference factors for the new user are: "+str(new_user_rating))
-
-    existing_users_rating = users_rating.existingUser(users,attributes)
+    #existing_users_rating = users_rating.existingUser(users, attributes)
     new_user_rating = users_rating.newUser(attributes)
 
     location = ['Amanora Park Town', 'Magarpatta City', 'Hadapsar', 'Mundhwa']
@@ -55,33 +47,27 @@ if __name__ == '__main__':
     flat_furnishing = ['Unfurnished', 'Semi-Furnished', 'Furnished']
 
     new_user_pref = []
-    new_user_pref.append(random.sample(location,1))
-    new_user_pref.append(random.sample(flat_type,1))
-    new_user_pref.append(random.sample(flat_furnishing,1))
+    new_user_pref.append(random.sample(location, 1))
+    new_user_pref.append(random.randint(7000,150000))
+    new_user_pref.append(random.sample(flat_type, 1))
+    new_user_pref.append(random.sample(flat_furnishing, 1))
 
-    print("\nThe preferred choices for the new user is:\n"+str(new_user_pref)+"\n")
+    print("\nThe preferred choices for the new user is:\n" + str(new_user_pref) + "\n")
 
-    #calculate eucledian distance and mean
-    eucledian_distance_list, mean, user_id = eucledian_distance.eucledian(existing_users_rating,new_user_rating)
-
-    #find the similar user profiles
-
-    #for debug Purpose
-    #similar_user_id_list = eucledian_distance.simUser(eucledian_distance_list,mean)
-
-    #sorted similar user list
-    sorted_sim_user_list = eucledian_distance.sortedSimUser(eucledian_distance_list,mean)
-
-
-
-    #users preference list
-    ########THIS IS FOR GENRATING THE USER PREFEREENCES SO THAT WE CAN ASSIGN THE TRAINING USERS TO A FLAT################
-    print("\nPreferred choices or selcted choice of the existing users:\n")
-    location_list = user_preference_data.locationList(location,users)
-    flat_type_list = user_preference_data.flatType(flat_type,users)
-    flat_furnishing_list = user_preference_data.flatFurnishing(flat_furnishing,users)
-
-
-
-
-
+        # calculate eucledian distance and mean
+    # eucledian_distance_list, mean, user_id = eucledian_distance.eucledian(existing_users_rating, new_user_rating)
+    #
+    #     # find the similar user profiles
+    #
+    #     # for debug Purpose
+    #     # similar_user_id_list = eucledian_distance.simUser(eucledian_distance_list,mean)
+    #
+    #     # sorted similar user list
+    # sorted_sim_user_list = eucledian_distance.sortedSimUser(eucledian_distance_list, mean)
+    #
+    #     # users preference list
+    #     ########THIS IS FOR GENRATING THE USER PREFEREENCES SO THAT WE CAN ASSIGN THE TRAINING USERS TO A FLAT################
+    # print("\nPreferred choices or selcted choice of the existing users:\n")
+    # location_list = user_preference_data.locationList(location, users)
+    # flat_type_list = user_preference_data.flatType(flat_type, users)
+    # flat_furnishing_list = user_preference_data.flatFurnishing(flat_furnishing, users)
