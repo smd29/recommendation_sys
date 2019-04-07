e34d92bbd6504697e557aa755aaed360fbabe633
smd29 <shreyasdas29@yahoo.com>
2019-04-07 13:47:25 +0530

    getting desired output

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import random
 # import word_similarity
 # import itertools
 import csv
+import heapq
 
 # consider location is the 1st attribute, flat_type is 2nd and flat_furnishing is 3rd
 if __name__ == '__main__':
-    Assigned_house = open(r"C:\Users\nEW u\Desktop\Recommendation system\Implementation\DataSet\Assigned_houses.csv")
+    Assigned_house = open(r"C:\Users\nEW u\Desktop\Recommendation system\Implementation\DataSet\Assigned_houses_updated.csv")
     csv_f = csv.reader(Assigned_house)
 
     rating_dataset =  open(r"C:\Users\nEW u\Desktop\Recommendation system\Implementation\DataSet\rating.csv")
@@ -57,10 +58,15 @@ if __name__ == '__main__':
     flat_type_rating.remove('Flat_type')
 
     existing_users_rating = []
-    for i in range(0,100):
+    for i in range(0,100): #as of now 100 is hard coded
         existing_users_rating.append([price_rating[i],location_rating[i],mode_of_sharing_rating[i],flat_type_rating[i]])
 
 
+    preferred_choices = []
+    for i in range(0,100):#as of now 100 is hard coded
+        #preferred_choices.append([user_id[i],location[i],price[i],size[i],flat_type[i],flat_id[i]])
+        preferred_choices.append([price[i],location[i],size[i],flat_type[i],flat_id[i]])
+    print(preferred_choices)
 
     users = len(user_id)
     attributes = 4
@@ -69,14 +75,17 @@ if __name__ == '__main__':
 
     #existing_users_rating = users_rating.existingUser(users, attributes)
     new_user_rating = users_rating.newUser(attributes)
+    sorted_index_of_newUser_rating = heapq.nlargest(4, range(len(new_user_rating)), key=new_user_rating.__getitem__)
+    print(sorted_index_of_newUser_rating)
 
     location = ['Amanora Park Town', 'Magarpatta City', 'Hadapsar', 'Mundhwa']
     flat_type = ['1BHK', '2BHK', '3BHK', '4BHK']
     flat_furnishing = ['Unfurnished', 'Semi-Furnished', 'Furnished']
+    price = ['below 20k','20k-40k','40k-60k','60k-80k','80k-100k','more than 100k']
 
     new_user_pref = []
-    new_user_pref.append(random.sample(location, 1))
-    new_user_pref.append(random.randint(7000,150000))
+    new_user_pref.append(random.sample(price, 1))
+    new_user_pref.append(random.sample(location,1))
     new_user_pref.append(random.sample(flat_type, 1))
     new_user_pref.append(random.sample(flat_furnishing, 1))
 
@@ -93,12 +102,28 @@ if __name__ == '__main__':
     #     # sorted similar user list
     sorted_sim_user_list = eucledian_distance.sortedSimUser(eucledian_distance_list, mean)
     top_ten_sim_user_list = sorted_sim_user_list[:10]
-    print(top_ten_sim_user_list)
-    #print(len(sorted_sim_user_list))
-    #
-    #     # users preference list
-    #     ########THIS IS FOR GENRATING THE USER PREFEREENCES SO THAT WE CAN ASSIGN THE TRAINING USERS TO A FLAT################
-    # print("\nPreferred choices or selcted choice of the existing users:\n")
-    # location_list = user_preference_data.locationList(location, users)
-    # flat_type_list = user_preference_data.flatType(flat_type, users)
-    # flat_furnishing_list = user_preference_data.flatFurnishing(flat_furnishing, users)
+    #print("Top 10 similar users are:\n"+str(top_ten_sim_user_list))
+
+################################################
+
+    for i in range(0,attributes):
+        print(new_user_pref[sorted_index_of_newUser_rating[i]][0])
+    #     #check if this value is similar for similar users or not
+
+    for i in range(0,len(top_ten_sim_user_list)):
+        print(preferred_choices[top_ten_sim_user_list[i]])
+
+    final_recommendation=[]
+
+    # for i in range(0,attributes):
+    #     for j in range(0,len(top_ten_sim_user_list)):
+    #         if(new_user_pref[sorted_index_of_newUser_rating[i]][0] == preferred_choices[top_ten_sim_user_list[j]]):
+    #             final_recommendation.append(preferred_choices[top_ten_sim_user_list[j]][-1])
+
+    print("Final recommendation generation")
+    for i in range(0,attributes):
+        for j in range(0,len(top_ten_sim_user_list)):
+            if(new_user_pref[sorted_index_of_newUser_rating[i]][0] in preferred_choices[top_ten_sim_user_list[j]]):
+                final_recommendation.append(preferred_choices[top_ten_sim_user_list[j]][-1])
+
+    print(final_recommendation)
